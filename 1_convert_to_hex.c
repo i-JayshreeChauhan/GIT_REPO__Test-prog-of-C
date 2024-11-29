aa47a94fa5e6570b24ff39bab1ff7d390e624c43
Jayshree Chauhan <jayshree.chauhan7397@gmail.com>
2024-11-29 11:43:41 +0530

    init commit

---
+++ 1_convert_to_hex.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int k=0;
+
+    printf("Enter a number : ");
+    scanf("%d",&k);
+    printf("\nDecimal value : %d  |  Hexadecimal value : 0x%X \n",k,k);
+
+}
